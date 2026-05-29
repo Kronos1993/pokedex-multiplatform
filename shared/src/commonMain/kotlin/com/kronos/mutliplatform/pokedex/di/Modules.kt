ae45f13e3452a31ac15ab5de1eafb14c31202ad8
kronos <mguerral1993@gmail.com>
2026-05-29 18:37:28 -0500

    feat: pokemon moves list added

--- shared/src/commonMain/kotlin/com/kronos/mutliplatform/pokedex/di/Modules.kt
+++ shared/src/commonMain/kotlin/com/kronos/mutliplatform/pokedex/di/Modules.kt
@@ -3,6 +3,7 @@ package com.kronos.mutliplatform.pokedex.di
 import com.kronos.mutliplatform.pokedex.features.move.list.MoveListScreenViewModel
 import com.kronos.mutliplatform.pokedex.features.pokedex.PokedexScreenViewModel
 import com.kronos.mutliplatform.pokedex.features.pokemon.detail.PokemonDetailScreenViewModel
+import com.kronos.mutliplatform.pokedex.features.pokemon.detail.pages.PokemonMovesTabViewModel
 import com.kronos.mutliplatform.pokedex.features.pokemon.list.PokemonListScreenViewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
@@ -13,4 +14,5 @@ val viewModelModule = module {
     viewModelOf(::MoveListScreenViewModel)
     viewModelOf(::PokemonListScreenViewModel)
     viewModelOf(::PokemonDetailScreenViewModel)
+    viewModelOf(::PokemonMovesTabViewModel)
 }
